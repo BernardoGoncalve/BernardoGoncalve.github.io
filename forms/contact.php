f34642cab01696cf2071ca24dc5d5e29eb4c271d
Bernardo Gonçalves <bernardobarrocasgoncalves12@gmail.com>
2024-10-26 19:25:10 +0100

    Pagina de Manutencao

--- forms/contact.php
+++ forms/contact.php
@@ -1,78 +1,7 @@
 <?php
-/**
- * Requires the "PHP Email Form" library
- * The "PHP Email Form" library is available only in the pro version of the template
- * The library should be uploaded to: vendor/php-email-form/php-email-form.php
- * For more info and help: https://bootstrapmade.com/php-email-form/
- */
+require 'vendor/autoload.php';  // Certifique-se de que o Composer carregou a biblioteca Mailjet
 
-//   // Replace contact@example.com with your real receiving email address
-//   $receiving_email_address = 'bernardobarrocasgoncalves12@gmail.com';
-
-//   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-//     include( $php_email_form );
-//   } else {
-//     die( 'Unable to load the "PHP Email Form" Library!');
-//   }
-
-//   $contact = new PHP_Email_Form;
-//   $contact->ajax = true;
-
-//   $contact->to = $receiving_email_address;
-//   $contact->from_name = $_POST['name'];
-//   $contact->from_email = $_POST['email'];
-//   $contact->subject = $_POST['subject'];
-
-//   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-//   /*
-//   $contact->smtp = array(
-//     'host' => 'example.com',
-//     'username' => 'example',
-//     'password' => 'pass',
-//     'port' => '587'
-//   );
-//   */
-
-//   $contact->add_message( $_POST['name'], 'From');
-//   $contact->add_message( $_POST['email'], 'Email');
-//   $contact->add_message( $_POST['message'], 'Message', 10);
-
-//   echo $contact->send();
-//
-?>
-
-<!-- <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $subject = htmlspecialchars($_POST['subject']);
-    $message = htmlspecialchars($_POST['message']);
-
-    // Configurações do e-mail
-    $to = 'bernardoprogramming@gmail.com';
-    $headers = "From: $email";
-
-    // Corpo do e-mail
-    $email_body = "Nome: $name\n";
-    $email_body .= "Email: $email\n";
-    $email_body .= "Assunto: $subject\n";
-    $email_body .= "Mensagem:\n$message\n";
-
-    // Enviar o e-mail
-    if (mail($to, $subject, $email_body, $headers)) {
-        echo 'Sua mensagem foi enviada. Obrigado!';
-    } else {
-        echo 'Houve um problema ao enviar sua mensagem. Tente novamente.';
-    }
-} else {
-    echo 'Método de solicitação inválido.';
-}
-?> -->
-
-<?php
-require 'vendor/autoload.php';
-
-use SendGrid\Mail\Mail;
+use \Mailjet\Resources;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
@@ -80,20 +9,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = htmlspecialchars($_POST['subject']);
     $message = htmlspecialchars($_POST['message']);
 
-    $email = new Mail();
-    $email->setFrom($email, $name);
-    $email->setSubject($subject);
-    $email->addTo('contact@example.com', 'Recipient');
-    $email->addContent('text/plain', $message);
+    // Configurações da API do Mailjet
+    $apiKey = '7f279028f2b9f95c0a7445e5db063a27';
+    $apiSecret = '88549da93104f2bfad7187ec3f08f6a5';
 
-    $sendgrid = new \SendGrid('YOUR_SENDGRID_API_KEY');
+    $mj = new \Mailjet\Client($apiKey, $apiSecret, true, ['version' => 'v3.1']);
+    
+    $body = [
+        'Messages' => [
+            [
+                'From' => [
+                    'Email' => $email,
+                    'Name' => $name
+                ],
+                'To' => [
+                    [
+                        'Email' => 'bernardoprogramming@gmail.com',
+                        'Name' => 'Recipient Name'
+                    ]
+                ],
+                'Subject' => $subject,
+                'TextPart' => $message,
+                'HTMLPart' => "<h3>$subject</h3><p>$message</p>",
+                'CustomID' => 'AppGettingStartedTest'
+            ]
+        ]
+    ];
 
-    try {
-        $response = $sendgrid->send($email);
+    $response = $mj->post(Resources::$Email, ['body' => $body]);
+
+    if ($response->success()) {
         echo 'Sua mensagem foi enviada. Obrigado!';
-    } catch (Exception $e) {
-        echo 'A mensagem não pode ser enviada. Erro: ' . $e->getMessage();
+    } else {
+        echo 'A mensagem não pode ser enviada. Erro: ' . $response->getReasonPhrase();
     }
 } else {
     echo 'Método de solicitação inválido.';
 }
+?>
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,11 +6,21 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Symfony\\Polyfill\\Php83\\' => array($vendorDir . '/symfony/polyfill-php83'),
+    'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
+    'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
+    'Symfony\\Contracts\\Translation\\' => array($vendorDir . '/symfony/translation-contracts'),
+    'Symfony\\Component\\Validator\\' => array($vendorDir . '/symfony/validator'),
     'SendGrid\\Stats\\' => array($vendorDir . '/sendgrid/sendgrid/lib/stats'),
     'SendGrid\\Mail\\' => array($vendorDir . '/sendgrid/sendgrid/lib/mail'),
     'SendGrid\\Helper\\' => array($vendorDir . '/sendgrid/sendgrid/lib/helper'),
     'SendGrid\\EventWebhook\\' => array($vendorDir . '/sendgrid/sendgrid/lib/eventwebhook'),
     'SendGrid\\Contacts\\' => array($vendorDir . '/sendgrid/sendgrid/lib/contacts'),
     'SendGrid\\' => array($vendorDir . '/sendgrid/php-http-client/lib'),
+    'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src', $vendorDir . '/psr/http-factory/src'),
+    'Psr\\Http\\Client\\' => array($vendorDir . '/psr/http-client/src'),
     'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
+    'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
+    'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
+    'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
 );
